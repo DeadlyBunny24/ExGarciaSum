cfda2a1e5f8d20e301e207c0b93942f6c3489fa4
deadlybunny <juajogar@espol.edu.ec>
2017-02-07 11:47:58 -0500

    Estado 2: Pruebas pasadas

--- GarciaSum/src/main/java/org/gradle/sum.java
+++ GarciaSum/src/main/java/org/gradle/sum.java
@@ -5,12 +5,12 @@ import static org.mockito.Mockito.*;
 public class sum {
 	public int sumador(String cadena){
 		int resultado = 0;
-		/*conv c = mock(conv.class);
+		conv c = mock(conv.class);
 		when(c.convertidor("2,3")).thenReturn(new int[]{2,3});
 		int[] arr = c.convertidor("2,3");
 		for(int i : arr) {       
 		    resultado += i;
-		} */
+		} 
 		return resultado;
 	}
 }
