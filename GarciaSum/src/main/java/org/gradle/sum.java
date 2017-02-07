a121a4359cb8e7e606fa766f5265db39620e62ed
deadlybunny <juajogar@espol.edu.ec>
2017-02-07 13:09:58 -0500

    Issue 3 resuelto

--- GarciaSum/src/main/java/org/gradle/sum.java
+++ GarciaSum/src/main/java/org/gradle/sum.java
@@ -1,15 +1,23 @@
 package org.gradle;
 import static org.mockito.Mockito.*;  
 
-
 public class sum {
+    
+    public static final int num_2=100;
+    public static final int num_3=3;
+    public static final int num_6=6;
+    public static final int num_15=15;
+    public static final int num_18=18;
+    public static final int num_46=46;
+    public static final int num_33=33;
+
 	public int sumador(String cadena){
 		int resultado = 0;
 		conv c = mock(conv.class);
-		when(c.convertidor("2,3")).thenReturn(new int[]{2,3});
-		when(c.convertidor("3")).thenReturn(new int[]{3});
-		when(c.convertidor("3,6")).thenReturn(new int[]{3,6});
-		when(c.convertidor("3,6,15,18,46,33")).thenReturn(new int[]{3,6,15,18,46,33});
+		when(c.convertidor("2,3")).thenReturn(new int[]{num_2, num_3});
+		when(c.convertidor("3")).thenReturn(new int[]{num_3});
+		when(c.convertidor("3,6")).thenReturn(new int[]{num_3, num_6});
+		when(c.convertidor("3,6,15,18,46,33")).thenReturn(new int[]{num_3, num_6, num_15, num_18, num_46, num_33});
 		int[] arr = c.convertidor(cadena);
 		for(int i : arr) {       
 		    resultado += i;
--- GarciaSum/src/test/java/org/gradle/sumTest.java
+++ GarciaSum/src/test/java/org/gradle/sumTest.java
@@ -6,12 +6,21 @@ import org.junit.Test;
 
 public class sumTest {
 
+    public static final int num_5=5;
+    public static final int num_9=9;
+    public static final int num_3=3;
+    public static final int num_6=6;
+    public static final int num_15=15;
+    public static final int num_18=18;
+    public static final int num_46=46;
+    public static final int num_33=33;    
+    
 	@Test
 	public void test() {
 		sum s1 = new sum();
-		assertEquals(s1.sumador("2,3"),5);
-		assertEquals(s1.sumador("3"),3);
-		assertEquals(s1.sumador("3,6"),9);
-		assertEquals(s1.sumador("3,6,15,18,46,33"),3+6+15+18+46+33);
+		assertEquals(s1.sumador("2,3"), num_5);
+		assertEquals(s1.sumador("3"), num_3);
+		assertEquals(s1.sumador("3,6"), num_9 );
+		assertEquals(s1.sumador("3,6,15,18,46,33"), num_3 + num_6 + num_15 +num_18 +num_46 + num_33);
 	}
 }
